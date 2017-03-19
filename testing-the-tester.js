79608d7be79440b79f096e1b40c75e3ed4900a3f
Jerel Lim <j.limster@gmail.com>
2017-03-19 17:03:04 +0800

    submit final

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,45 +1,38 @@
 //where i write javascript code
-possibleMoves = [0,1,2,3,4,5,6,7,8]
-totalMoves = []
+totalMoves = [0,0,0,0,0,0,0,0,0]
+player = 1
 
 function restart() {
-
-
-
+totalMoves = [0,0,0,0,0,0,0,0,0]
+player = 1
 }
 
 function isGameOver() {
-  return false
+  if (whoWon()) {
+    return true}
+return false
 }
 
-// (totalMoves.length%2 === 1) {
-//   totalMoves = []
-//   return 1
-
 function whoWon(){
-  if (totalMoves.length === 0) {
-    totalMoves = []
-    return 0
-  }
-  else if (totalMoves.length === 9) {
-    totalMoves = []
-    return 3
-  }
-  else if (totalMoves.length%2 === 1) {
-    totalMoves = []
-    return 1
-  }
-    else {
-    totalMoves = []
-    return 2
-  }
+  if (totalMoves[0] === totalMoves[1] && totalMoves[0] === totalMoves[2] && totalMoves[0]) return totalMoves[0]
+  else if (totalMoves[3] === totalMoves[4] && totalMoves[3] === totalMoves[5] && totalMoves[3]) return totalMoves[3]
+  else if (totalMoves[6] === totalMoves[7] && totalMoves[6] === totalMoves[8] && totalMoves[6]) return totalMoves[6]
+  else if (totalMoves[0] === totalMoves[3] && totalMoves[0] === totalMoves[6] && totalMoves[0]) return totalMoves[0]
+  else if (totalMoves[1] === totalMoves[4] && totalMoves[1] === totalMoves[7] && totalMoves[1]) return totalMoves[1]
+  else if (totalMoves[2] === totalMoves[5] && totalMoves[2] === totalMoves[8] && totalMoves[2]) return totalMoves[2]
+  else if (totalMoves[0] === totalMoves[4] && totalMoves[0] === totalMoves[8] && totalMoves[0]) return totalMoves[0]
+  else if (totalMoves[2] === totalMoves[4] && totalMoves[2] === totalMoves[6] && totalMoves[2]) return totalMoves[2]
+  else if (totalMoves[0] !==0 && totalMoves[1] !==0 && totalMoves[2] !== 0 && totalMoves[3]!== 0 && totalMoves[4] !== 0 &&
+    totalMoves[5] !== 0 && totalMoves[6] !== 0 && totalMoves[7] !== 0 && totalMoves[8] !== 0) return 3
+  else return 0
 }
 
 function playTurn(moves){
- if (possibleMoves.includes(moves) && !totalMoves.includes(moves)) {
-   totalMoves.push(moves)
+ if (totalMoves[moves] !== 0||isGameOver() && moves >totalMoves.length){
+    return false}
+ else {
+   totalMoves[moves] = player
+      if (player===1){player = 2}
+      else {player = 1}
    return true}
-else {
-  return false
-  }
 }
